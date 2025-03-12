b70bd3c58d23e1cd45c532c9e78e1a6d6ce0964f
jihyun <jihyun3066@naver.com>
2025-03-12 14:09:38 +0900

    Initial commit

---
+++ src/main/java/com/example/likelion_miniprojectgather/LikelionMiniprojectGatherApplication.java
@@ -0,0 +1,13 @@
+package com.example.likelion_miniprojectgather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionMiniprojectGatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionMiniprojectGatherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/likelion_miniprojectgather/LikelionMiniprojectGatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.likelion_miniprojectgather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionMiniprojectGatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
